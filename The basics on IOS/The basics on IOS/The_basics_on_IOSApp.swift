88e82fdce32a67ca00a7371db56cd8a69e1405de
Nguyen, Dennis Duy Quang <dnguyen448@gatech.edu>
2023-12-16 17:15:12 -0500

    Testing

--- The basics on IOS/The basics on IOS/The_basics_on_IOSApp.swift
+++ The basics on IOS/The basics on IOS/The_basics_on_IOSApp.swift
@@ -7,26 +7,11 @@
 
 import SwiftUI
 import SwiftData
+import Figlet
 
 @main
-struct The_basics_on_IOSApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
-    }
+struct FigletTool {
+  static func main() {
+    Figlet.say("Hello, Swift!")
+  }
 }
